121403ffc349dcac917a1f68b8031c896c7088f1
Cesar Cardoso <cesar.cardoso@suena.energy>
2026-05-17 15:34:41 +0200

    ai: Update Ralph

--- development/afk_ralph.sh
+++ development/afk_ralph.sh
@@ -7,13 +7,7 @@ if [ -z "$1" ]; then
 fi
 
 for ((i=1; i<=$1; i++)); do
-  cline --auto-approve true -v "@PRD.md @progress.txt \
-1. Read the @PRD.md and @progress.txt file. \
-2. Read ALL tasks. Find the next incomplete task and implement it. \
-3. Commit your changes in the local repository. \
-4. Update @progress.txt with what you did. \
-ONLY DO ONE TASK AT A TIME.
-  If the PRD is complete, output <promise>COMPLETE</promise>, otherwise, clean the session and run this same prompt again."
+  sbx run current-ai -- run --command "ralph" -m ollama/qwen3.5
 
   if [[ "$result" == *"<promise>COMPLETE</promise>"* ]]; then
     echo "PRD complete after $i iterations."
--- development/sbx_launcher.sh
+++ development/sbx_launcher.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 <iterations>"
-  exit 1
-fi
+#if [ -z "$1" ]; then
+  #echo "Usage: $0 <iterations>"
+  #exit 1
+#fi
 
 
 sbx rm current-ai || echo "Starting..."
 
-sbx create --name current-ai opencode $1
+sbx create --name current-ai opencode .
 sbx policy allow network current-ai localhost:11434
 sbx run current-ai
