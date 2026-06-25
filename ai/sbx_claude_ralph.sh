2a3d775f2ef1173cf61b2db44d22cef6b0c1f166
Cesar Cardoso <cesar.cardoso@suena.energy>
2026-06-25 18:15:28 +0200

    feat(ai): Add `ralph-loop` for claude

---
+++ ai/sbx_claude_ralph.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <iterations>"
+  exit 1
+fi
+
+mv ./.agents ./.claude | True
+
+if [ -z "$2" ]; then
+  sleeping_time=0
+else
+  sleeping_time=$2
+fi
+
+project_name="${PWD##*/}"
+project_name="${project_name//[^[:alnum:]-]/-}"
+sandbox_name="claude-${project_name}"
+
+for ((i=1; i<=$1; i++)); do
+  sbx run "$sandbox_name" -- --permission-mode acceptEdits "use skill ralph @.agents/PRD.md @.agents/issues @.agents/PROGRESS.md"
+  sleep $sleeping_time
+done
